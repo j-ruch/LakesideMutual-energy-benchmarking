c4dd087905794d57541b314bfda97ea3a367ae4d
Jan Ruch <j.ruch@hispeed.ch>
2025-04-10 23:27:03 +0800

    update sql file

--- sql/04_policymanagement.sql
+++ sql/04_policymanagement.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `addresses`
 --
 
+use policymanagement;
+
 DROP TABLE IF EXISTS `addresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
